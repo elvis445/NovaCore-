0693c96131d0d9eb6ed600a839f323c59bba7b5a
elvis445 <elvissedemdunyo@gmail.com>
2026-07-25 05:14:29 -0500

    Refactor wsgi.py to include web directory

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,11 @@
 import sys
 import os
 
-sys.path.insert(0,os.path.dirname(os.path.abspath(__file__)))
+base_dir=os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0,base_dir)
+sys.path.insert(0,os.path.join(base_dir,"web"))
+
+from app import app
 if __name__ == "__main__":
     app.run()
 
