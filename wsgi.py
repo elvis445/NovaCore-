44ba15e30d5edae3dd7e24f3aa4667bc44c6fc64
elvis445 <elvissedemdunyo@gmail.com>
2026-07-25 05:18:43 -0500

    Update WSGI app import and initialization
    Refactor import statements and adjust app initialization.

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,6 @@
-import sys
-import os
 
-base_dir=os.path.dirname(os.path.abspath(__file__)))
-sys.path.insert(0,base_dir)
-sys.path.insert(0,os.path.join(base_dir,"web"))
 
-from app import app
+from web.app import app
 if __name__ == "__main__":
     app.run()
 
