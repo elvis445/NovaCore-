90a1d31e3c15b9e3b91c7538cd52c720aeedafa8
elvis445 <elvissedemdunyo@gmail.com>
2026-07-24 22:52:23 -0500

    Add files via upload

---
+++ web/app.py
@@ -0,0 +1,26 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+
+
+from flask import Flask, render_template, request
+from brain.thinker import think
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+
+@app.route("/ask", methods=["POST"])
+def ask():
+    user = request.form['user']
+    answer = think(user)
+    return {"reply": answer}
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
