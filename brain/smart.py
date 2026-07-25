1992082cb83ab96f54c7deaf1b92635c90a76872
elvis445 <elvissedemdunyo@gmail.com>
2026-07-25 08:24:01 -0500

    Update smart.py

--- brain/smart.py
+++ brain/smart.py
@@ -1,10 +1,10 @@
 
 
 def smart_reply(user): 
+    user = user.lower().strip()
 
-
-    if "hello" in user or "hi" in user:
-        return "Hello! Elvis Nice to meet you today?  ."
+    if "hello" in ['hi',"hello","hey"]
+        return "Hello! Nice to meet you today?  ."
 
     if "how are you" in user:
         return "I am doing well, thank you for asking."
@@ -13,13 +13,13 @@ def smart_reply(user):
         return "My name is NovaCore,your personal AI assistance."
 
     if "who created you" in user:
-        return "I was created by Elvis."
+        return "I was created by Elvis Sedem Brown."
 
     if "what can you do" in user:
         return "I can chat,answer quesions,speak, and I am improving every day."
 
     if "good night" in user:
-        return "Good night,Elvis.Sleep well and keep dreaming."
+        return "Good night.Sleep well and keep dreaming."
 
     if "goodbye" in user:
         return "Goodbye! See you next time"
