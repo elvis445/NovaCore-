fcfb7db7f9893e2bf5c8a246c285b8242c558f9b
Elvis Sedem Dunyo <elvissedemdunyo@gmail.com>
2026-09-20 19:16:59 +0000

    Remove commented imports and clean up code

--- brain/thinker.py
+++ brain/thinker.py
@@ -123,5 +123,4 @@ def think(user):
         if user in chat['user']:
             return chat["bot"]
 
-    return "DEBUG: " +
-    str(smart_reply(user))
+        return "DEBUG: " + str(smart_reply(user))
