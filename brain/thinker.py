1326dd73ac900adda1160204d91f7c21613d09ab
Elvis Sedem Dunyo <elvissedemdunyo@gmail.com>
2026-09-20 19:56:20 +0000

    Update thinker.py

--- brain/thinker.py
+++ brain/thinker.py
@@ -27,6 +27,9 @@ def play_vedio(topic):
 def think(user):
     user = user.lower()
 
+        if user in ["hi", "hello", "hey"]:
+        return "Hello! I am NovaCore."
+
     if user.startswith("learn"):
         text = user.replace("learn", "")
 
